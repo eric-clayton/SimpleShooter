494c9a27663567f3a4f61157fb524e42e30d6440
Eric Clayton <Ericclayton1336@live.com>
2022-11-01 22:10:04 -0400

    Added win screen

--- Source/SimpleShooter/ShooterPlayerController.cpp
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -9,11 +9,22 @@ void AShooterPlayerController::GameHasEnded(AActor* EndGameFocus, bool bIsWinner
 {
 	Super::GameHasEnded(EndGameFocus, bIsWinner);
 
-	UUserWidget*LoseScreen = CreateWidget(this, LoseScreenClass);
-	if (LoseScreen != nullptr)
-	{
-		LoseScreen->AddToViewport();
-	}
 	
+	if (bIsWinner)
+	{
+		UUserWidget* WinScreen = CreateWidget(this, WinScreenClass);
+		if (WinScreen != nullptr)
+		{
+			WinScreen->AddToViewport();
+		}
+	}
+	else
+	{
+		UUserWidget* LoseScreen = CreateWidget(this, LoseScreenClass);
+		if (LoseScreen != nullptr)
+		{
+			LoseScreen->AddToViewport();
+		}
+	}
 	GetWorldTimerManager().SetTimer(RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
 }
